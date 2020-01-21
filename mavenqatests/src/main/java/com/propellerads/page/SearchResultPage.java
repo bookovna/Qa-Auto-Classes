7f46a6ae40362f14e79079780ca65a9875eb72cc
bookovna <58183120+bookovna@users.noreply.github.com>
2020-01-21 23:42:19 +0300

    New version

---
+++ mavenqatests/src/main/java/com/propellerads/page/SearchResultPage.java
@@ -0,0 +1,16 @@
+package com.propellerads.page;
+
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+
+public class SearchResultPage extends YandexMainPage {
+
+    private ElementsCollection searchResults = Selenide.$$("li.serp-item");
+
+    public void checkResultCount(int expectedCount) {
+        searchResults.shouldHave(CollectionCondition.sizeGreaterThan(expectedCount));
+    }
+
+}
